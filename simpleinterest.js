6bbc2c78dad3164cde5563cb19b5260d0e39d1b3
Aaron Harvey <aaron@sparxoo.com>
2016-11-28 10:55:18 -0500

    added prompts for rate and number of years to ensure the program will not let the user proceed without valid inputs.

--- simpleinterest.js
+++ simpleinterest.js
@@ -3,10 +3,18 @@ $(function() {
  var principle = Number(prompt("Enter the principle."));
 
  if (isNaN(principle)) {
-    parseInt(prompt("You did not enter a number. Please Enter the principle as a number."))
+    parseInt(prompt("You did not enter a number. Please enter the principle as a number."))
  }
  var interest = Number(prompt("Enter the rate of interest."));
+
+ if (isNaN(interest)) {
+    parseInt(prompt("You did not enter a number. Please enter the rate of interest as a number."))
+ }
  var time = Number(prompt("Enter the number of years."));
+
+ if (isNaN(time)) {
+    parseInt(prompt("You did not enter a number. Please enter the number of years as a number."))
+ }
  var investment = Number(principle*(1 + (interest / 100 * time)));
  document.write('After ' + time + ' years at ' + interest + '%, the investment will be worth $' + investment +'.')
 
