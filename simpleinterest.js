223e4a0ea7a7223300482476acde46611c26a188
Aaron Harvey <aaron@sparxoo.com>
2016-11-27 22:04:55 -0500

    changed console.log to document.write

--- simpleinterest.js
+++ simpleinterest.js
@@ -4,6 +4,6 @@ $(function() {
  var interest = Number(prompt("Enter the rate of interest."));
  var time = Number(prompt("Enter the number of years."))
  var investment = parseInt(principle*(1 + (interest * time)));
- console.log('After ' + time + ' years at ' + interest + '%, the investment will be worth $' + investment +'.')
- 
+ document.write('After ' + time + ' years at ' + interest + '%, the investment will be worth $' + investment +'.')
+
  })
