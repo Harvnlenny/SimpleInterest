ab5223da5572baca5668cecf3dc98bff51a5c499
Aaron Harvey <aaron@sparxoo.com>
2016-11-28 10:20:45 -0500

    added prompt if the user doesn't enter a number for the first question, found a missing semicolon, and adjusted the formula to divide interest by 100

--- simpleinterest.js
+++ simpleinterest.js
@@ -1,9 +1,13 @@
 $(function() {
 
  var principle = Number(prompt("Enter the principle."));
+
+ if (isNaN(principle)) {
+    parseInt(prompt("You did not enter a number. Please Enter the principle as a number."))
+ }
  var interest = Number(prompt("Enter the rate of interest."));
- var time = Number(prompt("Enter the number of years."))
- var investment = parseInt(principle*(1 + (interest * time)));
+ var time = Number(prompt("Enter the number of years."));
+ var investment = Number(principle*(1 + (interest / 100 * time)));
  document.write('After ' + time + ' years at ' + interest + '%, the investment will be worth $' + investment +'.')
 
  })
